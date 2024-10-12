14e7aa545151448fd08b85d00a70cf18d719e0fd
iisa47642 <136353118+iisa47642@users.noreply.github.com>
2024-10-12 23:57:02 +0300

    рппрпрпр

---
+++ app/goods/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        
+    def __str__(self):
+        return self.name
+        
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank = True, null = True, verbose_name = 'Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null = True, verbose_name='Изображение')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+    
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('id',)
+    
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
+    
+    def display_id(self):
+        return f'{self.id:05}'
+    
+    def sell_price(self):
+        if self.price_2:
+            return round(self.price - self.price_2*self.price/100,2)
+        
+        return self.price()
